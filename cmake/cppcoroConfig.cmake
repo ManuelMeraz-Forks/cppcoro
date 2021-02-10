927eda7e54b895130d40102e567dfd77c77ebfa3
Manuel Meraz <meraz.manuel@gmail.com>
2021-02-09 21:00:33 -0500

    find liburing as dependency in cppcoro config

--- cmake/cppcoroConfig.cmake
+++ cmake/cppcoroConfig.cmake
@@ -2,5 +2,6 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 include(CMakeFindDependencyMacro)
 find_dependency(Coroutines QUIET REQUIRED)
+find_dependency(liburing QUIET REQUIRED)
 
 include("${CMAKE_CURRENT_LIST_DIR}/cppcoroTargets.cmake")
